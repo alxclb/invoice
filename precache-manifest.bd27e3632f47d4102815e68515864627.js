a4fde9ff44e4f62b4e53b9e6823ab96276cfc076
Alexx <alexandar.radan@gmail.com>
2020-03-04 18:19:58 +0100

    Updates

---
+++ precache-manifest.bd27e3632f47d4102815e68515864627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b56168a86100dce9fbd893151b6836",
+    "url": "/invoiceSPA/index.html"
+  },
+  {
+    "revision": "694b796695e42ba29d1d",
+    "url": "/invoiceSPA/static/css/main.9789f62d.chunk.css"
+  },
+  {
+    "revision": "25d9b1cd27bf209b5d08",
+    "url": "/invoiceSPA/static/js/2.87dd672b.chunk.js"
+  },
+  {
+    "revision": "1c083a66ac878eb7f8b42e83a46e8c7d",
+    "url": "/invoiceSPA/static/js/2.87dd672b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "694b796695e42ba29d1d",
+    "url": "/invoiceSPA/static/js/main.b0b21f9b.chunk.js"
+  },
+  {
+    "revision": "7b69d46608e5f09a482a",
+    "url": "/invoiceSPA/static/js/runtime-main.0ada0512.js"
+  }
+]);
